e62afc8f1be09baec77264755e458bbb56a9ba70
Gui Rios <guilherme.rc.98@gmail.com>
2022-01-06 04:17:55 -0300

    🚚 Turn socket global

--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
 import { render } from 'react-dom'
+import { StrictMode } from 'react'
 import { Provider } from 'react-redux'
+import SocketContext, { socket } from './plugins/socket'
+import App from './routes'
 import store from './store'
-import App from './Routes'
 import './styles/main.scss'
 
-
 render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
+  <StrictMode>
+    <Provider store={store}>
+      <SocketContext.Provider value={socket}>
+        <App />
+      </SocketContext.Provider>
+    </Provider>
+  </StrictMode>,
   document.getElementById('root')
 );
