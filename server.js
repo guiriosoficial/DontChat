1b817c259ef671abdb919813ec76321e8de07f76
Gui Rios <guilherme.rc.98@gmail.com>
2022-01-08 06:56:49 -0300

    ✨ Implement nickname

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const { Server } = require('socket.io')
 const express = require('express')
-const cors = require('cors')
 const app = express()
 
+const cors = require('cors')
 app.use(cors())
 
 const server = require('http').createServer(app)
@@ -17,70 +17,76 @@ const io = new Server(server, {
 const clientsList = []
 
 io.on('connect', (socket) => {
-    // const userId = socket.id
-    console.log('connected', socket.id)
+    const userId = socket.id
+    console.log('connected', userId)
 
-    const findClient = () => clientsList.find(client => client.id === socket.id)
+    const findClient = () => clientsList.find(client => client.userId === userId)
 
-    const sendMessage = (message, messageType) => {
-        const { name, color } = findClient()
+    const sendMessage = (messageContent, messageType) => {
+        const { userName, userColor, roomPath } = findClient()
 
-        io.in(findClient()?.path).emit('RECIVE_MESSAGE', {
-            ...message,
+        io.in(roomPath).emit('RECIVE_MESSAGE', {
+            userId,
+            userName,
+            userColor,
             messageType,
+            messageContent,
             dateTime: new Date(),
-            userId: socket.id,
-            userName: name || socket.id,
-            userColor: color || 'rgb(0, 0, 0,)',
         })
     }
 
-    socket.on('JOIN_PATH', (path, name) => {
-        leavPath()
-        socket.join(path)
+    socket.on('CHANGE_USERNAME', (userName) => {
+        if (userName?.trim() && userName?.trim().length < 30) {
+            const clientIndex = clientsList.findIndex(client => client.userId === userId)
+            clientsList[clientIndex].userName = userName
+        } else {
+            socket.emit('CHANGE_USERNAME')
+        }
+    })
 
-        clientsList.push({
-            id: socket.id,
-            name,
-            path,
-            color: generateColor(path)
-        })
-        sendMessage({
-            userName: name,
-            messageContent: 'Joined room',
-            path
-        }, 'log')
-        
-        console.log(`User ${socket.id} joined ${path}`);
+    socket.on('JOIN_PATH', (roomPath, userName) => {
+        leavPath()
+
+        if (userName?.trim() && userName?.trim().length < 30) {
+            socket.join(roomPath)
+    
+            clientsList.push({
+                userId,
+                userName,
+                roomPath,
+                userColor: generateColor(roomPath)
+            })
+            sendMessage('Joined room', 'log')
+            
+            console.log(`User ${userId} joined ${roomPath}`);
+        } else {
+            socket.emit('CHANGE_USERNAME')
+        }
     })
     
     const leavPath = () => {
         if (findClient()) {
-            const { path, name } = findClient()?.path
+            const { roomPath } = findClient()
         
-            socket.leave(path);
-            sendMessage({
-                userName: name,
-                messageContent: 'Left room',
-                path
-            }, 'log')
-            clientsList.splice(socket.id, 1);
+            socket.leave(roomPath);
+            sendMessage('Left room', 'log')
+            clientsList.splice(userId, 1);
 
-            console.log(`User ${socket.id}, left ${path}`);
+            console.log(`User ${userId}, left ${roomPath}`);
         }
     }
 
-    socket.on('SEND_MESSAGE', (message) => {
-        sendMessage(message, 'message')
+    socket.on('SEND_MESSAGE', (messageContent) => {
+        sendMessage(messageContent, 'message')
     })
 
     socket.on('disconnect', () => {
         leavPath()
-        console.log('disconnected', socket.id)
+        console.log('disconnected', userId)
     })
 });
 
-const generateColor = (path) => {
+const generateColor = (roomPath) => {
     const getRandomColor = () => Math.random() * 255
     let r = getRandomColor()
     let g = getRandomColor()
@@ -96,11 +102,11 @@ const generateColor = (path) => {
 
     const color = `rgb(${r}, ${g}, ${b})`
 
-    const clientsRoom = clientsList.filter(client => client.path === path)
-    const clientsColor = clientsRoom.filter(client => client.color === color)
+    const clientsRoom = clientsList.filter(client => client.roomPath === roomPath)
+    const clientsColor = clientsRoom.filter(client => client.userColor === color)
 
     if (clientsColor.length) {
-        generateColor(path)
+        generateColor(roomPath)
     } else {
         return color
     }
