7473937b7d36369f6671ca9ce2a22e43af86d6c2
Gui Rios <guilherme.rc.98@gmail.com>
2022-01-08 03:13:34 -0300

    💄 Create dateTime filter

--- src/components/history/index.js
+++ src/components/history/index.js
@@ -1,4 +1,4 @@
-import { useEffect, useContext, useRef } from 'react'
+import React, { useEffect, useContext, useRef } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { updateMessages } from '../../store/messages'
 import SocketContext from '../../plugins/socket'
@@ -9,6 +9,18 @@ const History = () => {
   const socket = useContext(SocketContext)
   const messageRef = useRef(null)
   const dispatch = useDispatch()
+
+  const getDateTime = (dateTime) => {
+    const formatString = (value) => String(value).padStart(2, '0')
+    const now = new Date(dateTime)
+    const year = formatString(now.getFullYear())
+    const month = formatString(now.getMonth() + 1)
+    const day = formatString(now.getDate())
+    const hours = formatString(now.getHours())
+    const minutes = formatString(now.getMinutes())
+
+    return `${year}/${month}/${day} ${hours}:${minutes}`
+  }
 
   useEffect(() => {
     socket.on('RECIVE_MESSAGE', (message) => {
@@ -21,14 +33,17 @@ const History = () => {
     <section className='history'>
       <ul>
         {
-          messages.map(({ userId, userName, messageContent, dateTime }, index) => (
+          messages.map(({ userId, userName, userColor, messageContent, dateTime, messageType }, index) => (
             <li key={index} ref={messageRef}>
-              [{dateTime}]&nbsp;
-              <b>
-                {userName}
-                {userId === socket.id ? ' (You)' : ''}:&nbsp;
-              </b>
-              {messageContent}
+              <span style={{color: userColor}}>
+                [{getDateTime(dateTime)}]&nbsp;
+                <b>
+                  {userName}
+                  {userId === socket.id ? ' (You)' : ''}:
+                </b>
+              </span>
+              &nbsp;
+              {messageType === 'message' ? messageContent : <i>{messageContent}</i>}
             </li>
           ))
         }
