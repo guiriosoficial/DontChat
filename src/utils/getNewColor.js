371336d27882a992855b7f04b5d677fd2079ba87
Gui Rios <guilherme.rc.98@gmail.com>
2022-01-18 00:34:12 -0300

    ✨ Create color generator

---
+++ src/utils/getNewColor.js
@@ -0,0 +1,20 @@
+function getNewColor() {
+    const getNewNumber = () => Math.random() * 255
+    let r = getNewNumber()
+    let g = getNewNumber()
+    let b = getNewNumber()
+
+    const c = ((r * 0.299 + g * 0.587 + b * 0.114) / 255) - 0.6
+
+    if (c > 0) {
+      r = r * c
+      g = g * c
+      b = b * c
+    }
+
+    const rgbToHexa = (color) => Math.floor(color).toString(16).padStart(2, '0')
+
+    return `#${rgbToHexa(r)}${rgbToHexa(g)}${rgbToHexa(b)}`
+}
+
+export default getNewColor
