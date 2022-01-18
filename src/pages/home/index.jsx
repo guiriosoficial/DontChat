20bf32dea6401cea910ba707e45d1dc0ec00148e
Gui Rios <guilherme.rc.98@gmail.com>
2022-01-18 00:41:11 -0300

    🎨 Beatufy home

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -2,41 +2,36 @@ import React, { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import './style.scss'
 
-const Home = () => {
+function Home() {
+  const navigate = useNavigate()
   const [roomPath, setRoomPath] = useState('')
-  const handleChange = event => setRoomPath(event.target.value)
-  const navigate =  useNavigate()
+  const handleChangeRoomPath = (evt) => setRoomPath(evt.target.value)
 
   return (
     <main className="home">
       <h1>DONTCHAT</h1>
       <form onSubmit={() => navigate(roomPath)}>
-        <label htmlFor="path">www.dontchat.com/</label>
+        <label htmlFor="roomPath">www.dontchat.com/</label>
         &nbsp;
         <input
           value={roomPath}
-          type="text"
-          id="path"
-          name="path"
+          id="roomPath"
+          name="roomPath"
           autoCapitalize="off"
           autoCorrect="off"
-          onChange={handleChange}
+          type="text"
+          onChange={handleChangeRoomPath}
         />
         &nbsp;
         <button type="submit">Go!</button>
       </form>
       <section>
-        Dont login, just use a URL
-        <br />
-        Dont save any message or chat history
-        <br />
-        Dont pass any personal information
-        <br />
-        Dont lose your conversation, download with YourURL.zip
-        <br />
-        Dont forget, change your nickname whenever you want
-        <br />
-        Dontchat!
+        Dont login, just use a URL<br />
+        Dont save any message or chat history<br />
+        Dont pass any personal information<br />
+        Dont lose your conversation, download with YourURL.zip<br />
+        Dont forget, change your nickname whenever you want<br />
+        Dontchat!<br />
       </section>
     </main>
   )
