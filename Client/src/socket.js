cab73c9a2b96a9e287b540007fa125524e6b173f
Gui Rios <guilherme.rc.98@gmail.com>
2022-01-27 04:22:48 -0300

    👷 Insert envs and deploy script

--- Client/src/socket.js
+++ Client/src/socket.js
@@ -1,7 +1,7 @@
 import { createContext } from 'react'
 import io from 'socket.io-client'
 
-export const socket = io('http://localhost:3001')
+export const socket = io(process.env.HOST)
 const SocketContext = createContext(socket)
 
 export default SocketContext
