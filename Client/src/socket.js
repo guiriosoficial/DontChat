b99db3e39412c023b2eb0168a9b90c7ec27c6da3
Gui Rios <guilherme.rc.98@gmail.com>
2022-01-27 05:39:35 -0300

    🔊 Update logs

--- Client/src/socket.js
+++ Client/src/socket.js
@@ -1,8 +1,8 @@
 import { createContext } from 'react'
 import io from 'socket.io-client'
 
-const HOST = process.env.HOST || 'http://localhost:3001'
-console.log(HOST)
+const HOST = process.env.NODE_ENV || 'http://localhost:3001'
+console.log(process.env)
 
 export const socket = io(HOST)
 const SocketContext = createContext(socket)
