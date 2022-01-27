65cec3af178890760892a631b9ffd38f9891a68d
Gui Rios <guilherme.rc.98@gmail.com>
2022-01-27 05:31:51 -0300

    🔊 Add log to debug prod

--- Client/src/socket.js
+++ Client/src/socket.js
@@ -2,6 +2,7 @@ import { createContext } from 'react'
 import io from 'socket.io-client'
 
 const HOST = process.env.HOST || 'http://localhost:3001'
+console.log(HOST)
 
 export const socket = io(HOST)
 const SocketContext = createContext(socket)
--- Server/db.js
+++ Server/db.js
@@ -2,7 +2,7 @@ const { connect } = require('mongoose')
 
 const CONNECTION_STRING_URI = process.env.CONNECTION_STRING_URI || 'mongodb://localhost:27017/dontchat'
 
-const db = connect(CONNECTION_STRING_URI, { useNewUrlParser: true })
+const db = connect(CONNECTION_STRING_URI)
     .then(() => {
         console.log('MongoBD connected...')
     })
