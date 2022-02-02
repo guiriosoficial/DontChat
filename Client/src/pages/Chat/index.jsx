10336d2bdc9944bddf7d32311733906e75a1c070
Gui Rios <guilherme.rc.98@gmail.com>
2022-02-02 20:48:53 -0300

    🚚 Adjust git case bug

--- Client/src/pages/Chat/index.jsx
+++
@@ -1,7 +0,0 @@
-      </ChatContainer>
-      <ChatContainer>
-        {errorMessage && <ErrorMessage>{errorMessage}</ErrorMessage>}
-import { ChatContainer, ErrorMessage } from './styles'
-      <ChatContainer>
-        {errorMessage && <ErrorMessage>{errorMessage}</ErrorMessage>}
-      </ChatContainer>
